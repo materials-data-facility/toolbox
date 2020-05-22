59ea94c07fa303034b2e58d3cecf29b1972608bc
Logan Ward <ward.logan.t@gmail.com>
2020-05-22 13:32:23 -0500

    Moved utility into toolbox.py

--- tests/test_utils.py
+++
@@ -1,8 +0,0 @@
-"""Test for utility functions"""
-
-from mdf_toolbox.utils import rectify_path
-
-
-def test_rectify():
-    assert rectify_path('C:\\Users\\') == '/c/Users'
-    assert rectify_path('/users/test') == '/users/test'
